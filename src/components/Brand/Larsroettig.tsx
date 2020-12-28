4c3d02e3ce177597d2c6bd061dc0f1cb9c5479da
Lars <l.roettig@techdivision.com>
2020-12-28 12:56:32 +0100

    fix(cls): fix CLS Header

--- src/components/Brand/Larsroettig.tsx
+++ src/components/Brand/Larsroettig.tsx
@@ -3,7 +3,16 @@ import React from 'react';
 const LarsLogo: React.FC = () => (
   <div className="flex items-center">
     <a href="/" aria-label="back to homepage">
-      <img src="larsroettig.svg" alt="Logo from larsroettig.dev" />
+      <img
+        className="float-left"
+        src="larsroettig.svg"
+        alt="Logo from larsroettig.dev"
+        height="32"
+        width="32"
+      />
+      <span className="font-mono text-xl md:text-2xl font-extrabold tracking-tight hidden sm:inline">
+        Lars Roettig
+      </span>
     </a>
   </div>
 );
