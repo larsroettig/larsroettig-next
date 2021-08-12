8905efd20561ed655a58d334a963181c5a5942cc
Lars Roettig <l.roettig@techdivision.com>
2021-08-12 17:19:03 +0200

    Update next

--- src/components/Brand/Larsroettig.tsx
+++ src/components/Brand/Larsroettig.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
+import logo from './larsroettig-logo.svg';
 
 const LarsLogo: React.FC = () => (
   <span className="flex items-center">
-    <img
-      className="float-left"
-      src="/larsroettig-logo.svg"
-      alt="Logo from larsroettig.dev"
-      height="32"
-      width="32"
-    />
+    <Image src={logo} alt="Logo from larsroettig.dev" height="32" width="32" />
     <span className="font-mono text-xl md:text-2xl font-extrabold tracking-tight hidden lg:inline text-white">
       Lars Roettig
     </span>
